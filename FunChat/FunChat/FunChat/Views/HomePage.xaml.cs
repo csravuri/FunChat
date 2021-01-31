d7e0b89a1f8203b2a30a3957d6b7370014b57e7b
Chandra Sekhar Ravuri <chandrasekhar.r18@gmail.com>
2021-01-31 19:45:01 +0530

    Initial layout of HomePage completed

---
+++ FunChat/FunChat/FunChat/Views/HomePage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FunChat.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HomePage : TabbedPage
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Settings_Tapped(object sender, EventArgs e)
+        {
+            await DisplayAlert("Under Developement", "This feature is coming soon", "OK");
+        }
+
+        private async void Search_Tapped(object sender, EventArgs e)
+        {
+            await DisplayAlert("Under Developement", "This feature is coming soon", "OK");
+        }
+
+        private async void UserIcon_Tapped(object sender, EventArgs e)
+        {
+            await DisplayAlert("Under Developement", "This feature is coming soon", "OK");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # FunChat
 Personal chat application, may be with encryption and more
+
+
+# Resources
+* Icons @ https://icons8.com/
